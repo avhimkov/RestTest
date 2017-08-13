bc2f3ced402adfb528f4de4fa475450f772d8f83
Dmitry <avhimkov@mail.ru>
2017-08-13 19:01:09 +0500

    refactoring rest function Get, Create, Delete, Update

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"net/http"
+	"encoding/json"
 )
 func main()  {
 	router := mux.NewRouter()
@@ -22,19 +23,34 @@ func main()  {
 }
 
 func GetPeople(w http.ResponseWriter, r *http.Request)  {
-	
+	json.NewEncoder(w).Encode(people)
 }
 
 func GetPerson(w http.ResponseWriter, r *http.Request)  {
-
+	params := mux.Vars(r)
+	for _ , item := range people {
+		if item.ID == params["id"]
+	}
 }
 
 func CreatePerson(w http.ResponseWriter, r *http.Request)  {
-
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 func DeletePerson(w http.ResponseWriter, r *http.Request)  {
-
+	params := mux.Vars(r)
+	for index, item := range people{
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index + 1]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
 
 type Person struct {
