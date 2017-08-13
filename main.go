76b04cff3ada09d071bbdf24453b778c73c20a8b
Dmitry <avhimkov@mail.ru>
2017-08-13 18:26:14 +0500

    crate func GetPeople, GetPerson, CreatePerson, DeletePerson

--- main.go
+++ main.go
@@ -11,5 +11,26 @@ import (
 
 func main()  {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetPeople(w http.ResponseWriter, r *http.Request)  {
+	
+}
+
+
+func GetPerson(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request)  {
+
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request)  {
+
 }
