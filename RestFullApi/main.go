deb65f3e67eb1897994171ac06be71e2eb6d581e
Dmitry <avhimkov@mail.ru>
2017-08-14 19:45:10 +0500

    v1 RestFullApi

--- RestFullApi/main.go
+++ RestFullApi/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	//"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine  {
+	router := gin.Default()
+	v1 := router.Group("api/v1")
+	{
+		v1.GET("/instructions", GetInstruction)
+	}
+	return router
+}
+
+func GetInstruction(c *gin.Context)  {
+	c.JSON(200, gin.H{"ok": "Welcome to Hell!"})
+}
 
 func main() {
-	fmt.Println("Hello")
+	router := SetupRouter()
+	router.Run(":8080")
 }
