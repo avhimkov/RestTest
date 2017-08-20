85280230e2b0502d28e539a48f468a7a53dd945c
Dmitry <avhimkov@mail.ru>
2017-08-20 17:18:11 +0500

    refactoring dao.go

--- RestMongo/dao/dao.go
+++ RestMongo/dao/dao.go
@@ -1,11 +1,11 @@
 package dao
 
 import (
-"log"
+	"log"
 
-"github.com/mlabouardy/movies-restapi/models"
-mgo "gopkg.in/mgo.v2"
-"gopkg.in/mgo.v2/bson"
+	"github.com/mlabouardy/movies-restapi/models"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type MoviesDAO struct {
@@ -25,4 +25,31 @@ func (m *MoviesDAO) Connect() {
 		log.Fatal(err)
 	}
 	db = session.DB(m.Database)
+}
+
+func (m *MoviesDAO) FindAll() ([]Movie, error) {
+	var movies []Movie
+	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
+	return movies, err
+}
+
+func (m *MoviesDAO) FindById(id string) (Movie, error) {
+	var movie Movie
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
+	return movie, err
+}
+
+func (m *MoviesDAO) Insert(movie Movie) error {
+	err := db.C(COLLECTION).Insert(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Delete(movie Movie) error {
+	err := db.C(COLLECTION).Remove(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Update(movie Movie) error {
+	err := db.C(COLLECTION).UpdateId(movie.ID, &movie)
+	return err
 }
