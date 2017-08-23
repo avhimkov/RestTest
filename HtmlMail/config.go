2fd9df9e3aa5323c33bef738369368f0c5700899
Dmitry <avhimkov@mail.ru>
2017-08-23 16:51:05 +0500

    project HtmlMail add config.go

---
+++ HtmlMail/config.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Server		string
+	Port		int
+	Email		string
+	Password	string
+}
+
+func (c *Config) Read() {
+	if _, err := toml.DecoderFile("config.tolm", &c); err !=nil{
+		log.Fatal(err)
+	}
+}
