6ba40cb4d07d7b486034f5f891d5270ce667b164
Dmitry <avhimkov@mail.ru>
2017-08-27 21:59:57 +0500

    GoroutAndCha project refactoring func worker

--- GoroutAndCha/main.go
+++ GoroutAndCha/main.go
@@ -12,4 +12,19 @@ var data chan string
 
 func main() { }
 
-func worker() { }
+func worker() {
+	fmt.Println("Goroutine worker is now starting...")
+	defer func() {
+		fmt.Println("Destroying the worker...")
+		waitGroup.Done()
+	}()
+	for {
+		value, ok := <-data
+		if !ok {
+			fmt.Println("The channel is closed!")
+			break
+		}
+		fmt.Println(value)
+		time.Sleep(time.Second * 1)
+	}
+}
